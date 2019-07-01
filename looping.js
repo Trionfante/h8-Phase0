042800dc68c5ce21bd6a840e705488c120ed22ee
Melvin Hartan <melv_hartan@live.com>
2019-07-01 10:19:37 +0700

    looping.js

---
+++ looping.js
@@ -0,0 +1,14 @@
+//looping
+
+var i=0;
+console.log("increase\n");
+while (i < 21){
+    console.log("uwuwuw " + i);
+    i += 2;
+}
+var i=20;
+console.log("\ndecrease\n");
+while (i > 0){
+    console.log("wuwuwu " + i);
+    i -= 2;
+}
