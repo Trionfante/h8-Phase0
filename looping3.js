35d76d0f9a84eabc578b37f1b0644795577974a7
Trionfante <melv_hartan@live.com>
2019-07-11 20:17:12 +0700

    update

---
+++ looping3.js
@@ -0,0 +1,28 @@
+//looping asterisk
+
+var rows1 = 5
+var rows2 = 5
+var rows3 = 5
+var stara = ''
+var starb = ''
+
+while (rows1 > 0){
+    rows1--;
+    console.log('*');
+}
+console.log('\n');
+
+while (rows2 > 0){
+    rows2--;
+    for (count=5;count>0;count--){
+        stara+='*';
+    }
+    console.log(stara)
+    stara = '';
+}
+console.log('\n');
+
+while (rows3 > 0){
+    rows3--;
+    console.log(starb+='*');
+}
