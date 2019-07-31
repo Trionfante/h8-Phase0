4488383721c2176667f66f034bf2208b7049faca
Trionfante <melv_hartan@live.com>
2019-07-31 17:49:35 +0700

    exercise 1 and 3

---
+++ exercise-1.js
@@ -0,0 +1,67 @@
+// 1. Newton's Second Law
+    
+    // Algorithm
+    1. Initiate equation for the Law, which is F equals to m multiplied by a.
+    2. Multiply m with a.
+    3. Put the result of the multiplication to F.
+    4. Print F.
+
+    // Pseudocode
+    STORE m
+    STORE a
+    STORE F
+    SET F equals to m multiplied by a
+    PRINT F
+
+// 2. Tahun Kabisat
+
+    // Algorithm
+    1. Initiate value of Year.
+    2. Check if Year is divisible by 4, but not divisible by 100. If both conditions are true, go to step 4. If not, go to step 3.
+    3. Check if Year is divisible by 4, 100 and 400. If is is then go to step 4. If it is not, then go to step 5.
+    4. Tell the world that it is a Leap Year.
+    5. Tell the world that it is a Common Year.
+
+    //Pseudocode
+    STORE year
+    IF year MOD 4 EQUALS 0 AND year MOD 100 INEQUALS 0
+        PRINT leap
+    ELSE IF year MOD 4 EQUALS 0 AND year MOD 100 EQUALS 0 AND year MOD 400 EQUALS 0
+        PRINT leap
+    ELSE
+        PRINT common
+
+// 3. Laundry Day
+    
+    // Algorithm
+    1. Initiate total amount of laundry to wash.
+    2. Count the number as you put laundry in until it reach the total amount from step 1.
+    3. After the count reach total number of laundry, turn the washing machine.
+
+    //Pseudocode
+    STORE total
+    STORE count
+    WHILE count LESS THAN total
+        count PLUS 1
+    PRINT done
+
+// 3. Periksa Kuku
+
+    // Algorithm
+    1. Initiate total number of student.
+    2. Start counting as you check the student.
+    3. While the count is not equal to total number, check the next student.
+    4. If the nail is short, praise the student. else go to step 5.
+    5. Punish the student.
+
+    // Pseudocode
+    STORE total
+    STORE count
+    STORE short
+    WHILE count LESS THAN total
+        count PLUS 1
+        IF short equals TRUE
+            PRINT good
+        ELSE 
+            PRINT bad
+    PRINT done
