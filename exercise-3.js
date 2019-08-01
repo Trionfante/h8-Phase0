6c2f1fcdf55a2715dc9c139a24662b648befa3b7
Trionfante <melv_hartan@live.com>
2019-08-01 16:02:14 +0700

    update exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,8 +1,27 @@
 //if else
 
+/*OBSOLETE
+var nama = 'Mikael'
+var peran = ''
+
+if (nama == '' && peran == ''){
+  console.log("Nama harus diisi!");
+} else if (nama == 'Mikael' && peran == ''){
+  console.log('Halo Mikael, pilih peranmu untuk memulai!');
+} else if (nama == 'Nina' && peran == 'Ksatria'){
+  console.log('Halo '+nama+',');
+  console.log('Selamat datang '+peran+', kamu dapat menyerang musuh!');
+} else if (nama == 'Danu' && peran == 'Tabib'){
+  console.log('Halo '+nama+',');
+  console.log('Selamat datang '+peran+', kamu dapat mengobati teman!');
+} else if (nama == 'Zero' && peran == 'Penyihir'){
+  console.log('Halo '+nama+',');
+  console.log('Selamat datang '+peran+', kamu dapat membantu teman!');
+}
+*/
+
 var nama = 'Mikael';
 var peran = 'Ksatria';
-
 
 
 if (nama == '' && peran == ''){
