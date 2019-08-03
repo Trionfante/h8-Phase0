97e5260113ba73841be0db8e4e7bb15806eab856
Trionfante <melv_hartan@live.com>
2019-08-03 16:43:39 +0700

    tiddying up with section

--- exercise-7.js
+++ exercise-7.js
@@ -6,28 +6,39 @@ var rows3 = 5
 var starA = ''
 // var starB = ''
 
-while (rows1 > 0){
-    rows1--;
-    console.log('*');
-}
-
-console.log();
-
-while (rows2 > 0){
-    rows2--;
-    for (count = 5; count > 0; count--){
-        starA += '*';
+// 1. Menyusun Barisan Bintang
+    while (rows1 > 0){
+        rows1--;
+        console.log('*');
     }
-    console.log(starA)
-    starA = '';
-}
 
-console.log();
+// End of Barisan Bintang
 
-while (rows3 > 0){
-    rows3--;
-    //console.log(starb+='b');
-    for (count = 1; count > 0; count--){
-    console.log(starA+='*');
+    console.log();
+
+// 2. Menyusun Barisan Bintang dengan Nested Looping
+
+    while (rows2 > 0){
+        rows2--;
+        for (count = 5; count > 0; count--){
+            starA += '*';
+        }
+        console.log(starA)
+        starA = '';
     }
-}
+
+// End of Barisan dengan Nested Looping
+
+    console.log();
+
+// 3. Menyusun Barisan Tangga Bintang dengan Nested Looping
+
+    while (rows3 > 0){
+        rows3--;
+        //console.log(starb+='b');
+        for (count = 1; count > 0; count--){
+        console.log(starA+='*');
+        }
+    }
+
+// End of Barisan Tangga Bintang
